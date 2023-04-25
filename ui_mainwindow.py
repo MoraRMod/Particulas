257bb2efed473c5e0c7830612c4cb9cb928f11cc
Omar Díaz <ralerwip@gmail.com>
2023-04-24 23:31:42 -0600

    Se realizo el cambio de la interfazy se transformo a codigo python.

--- ui_mainwindow.py
+++ ui_mainwindow.py
@@ -13,21 +13,23 @@ from PySide2.QtGui import *
 from PySide2.QtWidgets import *
 
 
-class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        if not Dialog.objectName():
-            Dialog.setObjectName(u"Dialog")
-        Dialog.resize(709, 499)
-        self.actionAbrir = QAction(Dialog)
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 600)
+        self.actionAbrir = QAction(MainWindow)
         self.actionAbrir.setObjectName(u"actionAbrir")
-        self.actionGuardar = QAction(Dialog)
+        self.actionGuardar = QAction(MainWindow)
         self.actionGuardar.setObjectName(u"actionGuardar")
-        self.actionGuardar.setCheckable(False)
-        self.Particulas_GroupBox = QGroupBox(Dialog)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.gridLayout_3 = QGridLayout(self.centralwidget)
+        self.gridLayout_3.setObjectName(u"gridLayout_3")
+        self.Particulas_GroupBox = QGroupBox(self.centralwidget)
         self.Particulas_GroupBox.setObjectName(u"Particulas_GroupBox")
-        self.Particulas_GroupBox.setGeometry(QRect(0, 0, 711, 501))
-        self.gridLayout = QGridLayout(self.Particulas_GroupBox)
-        self.gridLayout.setObjectName(u"gridLayout")
+        self.gridLayout_2 = QGridLayout(self.Particulas_GroupBox)
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
         self.Datos_GroupBox = QGroupBox(self.Particulas_GroupBox)
         self.Datos_GroupBox.setObjectName(u"Datos_GroupBox")
         self.Blue_SpinBox = QSpinBox(self.Datos_GroupBox)
@@ -68,58 +70,70 @@ class Ui_Dialog(object):
         self.DestinoX_Label = QLabel(self.Datos_GroupBox)
         self.DestinoX_Label.setObjectName(u"DestinoX_Label")
         self.DestinoX_Label.setGeometry(QRect(10, 20, 51, 21))
-        self.Valocidad_SpinBox = QSpinBox(self.Datos_GroupBox)
-        self.Valocidad_SpinBox.setObjectName(u"Valocidad_SpinBox")
-        self.Valocidad_SpinBox.setGeometry(QRect(20, 140, 51, 22))
+        self.Velocidad_SpinBox = QSpinBox(self.Datos_GroupBox)
+        self.Velocidad_SpinBox.setObjectName(u"Velocidad_SpinBox")
+        self.Velocidad_SpinBox.setGeometry(QRect(20, 140, 51, 22))
 
-        self.gridLayout.addWidget(self.Datos_GroupBox, 0, 0, 2, 1)
+        self.gridLayout_2.addWidget(self.Datos_GroupBox, 0, 0, 2, 1)
 
         self.Contenido_PlainTextEdit = QPlainTextEdit(self.Particulas_GroupBox)
         self.Contenido_PlainTextEdit.setObjectName(u"Contenido_PlainTextEdit")
 
-        self.gridLayout.addWidget(self.Contenido_PlainTextEdit, 0, 1, 1, 3)
+        self.gridLayout_2.addWidget(self.Contenido_PlainTextEdit, 0, 1, 1, 3)
 
         self.AgregarInicio_PushButton = QPushButton(self.Particulas_GroupBox)
         self.AgregarInicio_PushButton.setObjectName(u"AgregarInicio_PushButton")
 
-        self.gridLayout.addWidget(self.AgregarInicio_PushButton, 1, 1, 1, 1)
+        self.gridLayout_2.addWidget(self.AgregarInicio_PushButton, 1, 1, 1, 1)
 
         self.AgregarFinal_PushButton = QPushButton(self.Particulas_GroupBox)
         self.AgregarFinal_PushButton.setObjectName(u"AgregarFinal_PushButton")
 
-        self.gridLayout.addWidget(self.AgregarFinal_PushButton, 1, 2, 1, 1)
+        self.gridLayout_2.addWidget(self.AgregarFinal_PushButton, 1, 2, 1, 1)
 
         self.Mostrar_PushButon = QPushButton(self.Particulas_GroupBox)
         self.Mostrar_PushButon.setObjectName(u"Mostrar_PushButon")
 
-        self.gridLayout.addWidget(self.Mostrar_PushButon, 1, 3, 1, 1)
+        self.gridLayout_2.addWidget(self.Mostrar_PushButon, 1, 3, 1, 1)
 
 
-        self.retranslateUi(Dialog)
+        self.gridLayout_3.addWidget(self.Particulas_GroupBox, 0, 0, 1, 1)
 
-        QMetaObject.connectSlotsByName(Dialog)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 800, 21))
+        self.menuArchivo = QMenu(self.menubar)
+        self.menuArchivo.setObjectName(u"menuArchivo")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.menubar.addAction(self.menuArchivo.menuAction())
+        self.menuArchivo.addAction(self.actionAbrir)
+        self.menuArchivo.addAction(self.actionGuardar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
     # setupUi
 
-    def retranslateUi(self, Dialog):
-        Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"Dialog", None))
-        self.actionAbrir.setText(QCoreApplication.translate("Dialog", u"Abrir", None))
-#if QT_CONFIG(shortcut)
-        self.actionAbrir.setShortcut(QCoreApplication.translate("Dialog", u"Ctrl+A", None))
-#endif // QT_CONFIG(shortcut)
-        self.actionGuardar.setText(QCoreApplication.translate("Dialog", u"Guardar", None))
-#if QT_CONFIG(shortcut)
-        self.actionGuardar.setShortcut(QCoreApplication.translate("Dialog", u"Ctrl+G", None))
-#endif // QT_CONFIG(shortcut)
-        self.Particulas_GroupBox.setTitle(QCoreApplication.translate("Dialog", u"Particulas", None))
-        self.Datos_GroupBox.setTitle(QCoreApplication.translate("Dialog", u"Datos", None))
-        self.Green_Label.setText(QCoreApplication.translate("Dialog", u"Green", None))
-        self.Red_Label.setText(QCoreApplication.translate("Dialog", u"Red", None))
-        self.Blue_Label.setText(QCoreApplication.translate("Dialog", u"Blue", None))
-        self.DestinoY_Label.setText(QCoreApplication.translate("Dialog", u"Destino Y", None))
-        self.Velocidad_Label.setText(QCoreApplication.translate("Dialog", u"Velocidad", None))
-        self.DestinoX_Label.setText(QCoreApplication.translate("Dialog", u"Destino X", None))
-        self.AgregarInicio_PushButton.setText(QCoreApplication.translate("Dialog", u"Agregar Inicio", None))
-        self.AgregarFinal_PushButton.setText(QCoreApplication.translate("Dialog", u"Agregar Final", None))
-        self.Mostrar_PushButon.setText(QCoreApplication.translate("Dialog", u"Mostrar", None))
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.actionAbrir.setText(QCoreApplication.translate("MainWindow", u"Abrir", None))
+        self.actionGuardar.setText(QCoreApplication.translate("MainWindow", u"Guardar", None))
+        self.Particulas_GroupBox.setTitle(QCoreApplication.translate("MainWindow", u"Particulas", None))
+        self.Datos_GroupBox.setTitle(QCoreApplication.translate("MainWindow", u"Datos", None))
+        self.Green_Label.setText(QCoreApplication.translate("MainWindow", u"Green", None))
+        self.Red_Label.setText(QCoreApplication.translate("MainWindow", u"Red", None))
+        self.Blue_Label.setText(QCoreApplication.translate("MainWindow", u"Blue", None))
+        self.DestinoY_Label.setText(QCoreApplication.translate("MainWindow", u"Destino Y", None))
+        self.Velocidad_Label.setText(QCoreApplication.translate("MainWindow", u"Velocidad", None))
+        self.DestinoX_Label.setText(QCoreApplication.translate("MainWindow", u"Destino X", None))
+        self.AgregarInicio_PushButton.setText(QCoreApplication.translate("MainWindow", u"Agregar Inicio", None))
+        self.AgregarFinal_PushButton.setText(QCoreApplication.translate("MainWindow", u"Agregar Final", None))
+        self.Mostrar_PushButon.setText(QCoreApplication.translate("MainWindow", u"Mostrar", None))
+        self.menuArchivo.setTitle(QCoreApplication.translate("MainWindow", u"Archivo", None))
     # retranslateUi
 
