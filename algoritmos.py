8e9fcda6f1b398f2c56e9b223f940c07643c55d2
MoraRMod <ralerwip@gmail.com>
2023-05-05 17:36:47 -0600

    No recuerdo que le movi

--- algoritmos.py
+++ algoritmos.py
@@ -3,7 +3,7 @@ import math
 def distanciaEuclideana(x_1, y_1, x_2, y_2):
     return math.sqrt(math.pow((x_2 - x_1), 2) + math.pow((y_2 - y_1), 2))
 
-def puntosMasCarcanos(puntos_list) -> list:
+def puntosMasCercanos(puntos_list) -> list:
     resultado = []
 
     for punto_i in puntos_list:
