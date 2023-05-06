469c7a7bf6bf3f1072dc170034ed029bbf021356
MoraRMod <ralerwip@gmail.com>
2023-05-05 19:31:26 -0600

    Se modifica la funcion de puntos mas cercanos, creo que esta bien asi

--- algoritmos.py
+++ algoritmos.py
@@ -6,22 +6,33 @@ def distanciaEuclideana(x_1, y_1, x_2, y_2):
 def puntosMasCercanos(puntos_list) -> list:
     resultado = []
 
-    for punto_i in puntos_list:
-        origenX = punto_i[0]
-        origenY = punto_i[1]
+    for puntoI in puntos_list:
+        one_origenX = puntoI[0]
+        one_origenY = puntoI[1]
+        one_destinoX = puntoI[2]
+        one_destinoY = puntoI[3]
+
         min = 1000
         cercano = (0,0)
 
-        for punto_j in puntos_list:
-            if punto_i != punto_j:
-                destinoX = punto_j[0]
-                destinoY = punto_j[1]
-                d = distanciaEuclideana(origenX, origenY, destinoX, destinoY)
+        for puntoJ in puntos_list:
+            if puntoI != puntoJ:
+                two_origenX = puntoJ[0]
+                two_origenY = puntoJ[1]
+                two_destinoX = puntoJ[2]
+                two_destinoY = puntoJ[3]
 
-                if d < min:
-                    min = d
-                    cercano = (destinoX, destinoY)
+                origen = distanciaEuclideana(one_origenX, one_origenY, two_origenX, two_origenY)
+                destino = distanciaEuclideana(one_destinoX, one_destinoY, two_destinoX, two_destinoY)
+
+                if origen < min:
+                    min = origen
+                    cercano = (two_origenX, two_origenY, two_destinoX, two_destinoY)
+
+                if destino < min:
+                    min = destino
+                    cercano = (two_destinoX, two_destinoY, two_origenX, two_origenY)
         
-        resultado.append((punto_i, cercano))
+        resultado.append((puntoI, cercano))
     
     return resultado
